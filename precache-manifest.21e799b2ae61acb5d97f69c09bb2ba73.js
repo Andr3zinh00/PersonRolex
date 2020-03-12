7e95b26f127efc8d592f7c0b0cae7ad051b6ac01
André Luiz <andrelp1015@gmail.com>
2020-03-12 00:32:24 -0300

    Updates

---
+++ precache-manifest.21e799b2ae61acb5d97f69c09bb2ba73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f04a6935e5430dfd54ee2ce87e1de38",
+    "url": "/PersonRolex/index.html"
+  },
+  {
+    "revision": "063d3f396cbb80803373",
+    "url": "/PersonRolex/static/css/main.78bca910.chunk.css"
+  },
+  {
+    "revision": "cdeb9dff36fea608468b",
+    "url": "/PersonRolex/static/js/2.6e2f278f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/PersonRolex/static/js/2.6e2f278f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063d3f396cbb80803373",
+    "url": "/PersonRolex/static/js/main.b4d8296c.chunk.js"
+  },
+  {
+    "revision": "974593fc901ffbc3a1b0",
+    "url": "/PersonRolex/static/js/runtime-main.d3c1dbee.js"
+  }
+]);
